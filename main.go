2e662b6e687e24439aa9792e30f7548890bd72fe
WOsaka <wresinski@gmx.net>
2025-06-07 08:00:26 +0200

    custom handlers

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8", )
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("OK"))
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server{
 		Handler: mux,
