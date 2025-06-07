d23405c4934894307b5d573f27d85df9dedf4355
WOsaka <wresinski@gmx.net>
2025-06-07 17:25:51 +0200

    stateful handlers

--- main.go
+++ main.go
@@ -3,24 +3,54 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8", )
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
 	w.WriteHeader(http.StatusOK)
 
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	hitsStr := "Hits: " + strconv.Itoa(int(hits))
+	w.Write([]byte(hitsStr))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Hits counter reset"))
+}
+
 func main() {
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", readinessHandler)
+	mux.Handle(
+		"/app/",
+		http.StripPrefix("/app",
+			cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", healthCheckHandler)
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
 
 	server := &http.Server{
 		Handler: mux,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
 
 	fmt.Println("Server listening on http://localhost:8080")
