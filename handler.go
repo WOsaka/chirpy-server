6a4d6e1877a5d3f0e5c880bed26aca39cf81bb24
WOsaka <wresinski@gmx.net>
2025-06-08 20:00:16 +0200

    json

---
+++ handler.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	hits := cfg.fileserverHits.Load()
+	htmlTempl := `
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>
+	`
+	html := fmt.Sprintf(htmlTempl, hits)
+	w.Write([]byte(html))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Hits counter reset"))
+}
+
+func validationHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	type validationResp struct {
+		Error string `json:"error,omitempty"`
+		Valid bool   `json:"valid,omitempty"`
+	}
+
+	var respBody validationResp
+	if len(params.Body) > 140 {
+		respBody.Error = "Chirp is to long"
+		w.WriteHeader(400)
+	} else {
+		respBody.Valid = true
+		w.WriteHeader(200)
+	}
+
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dat)
+}
+
+// respondWithError(w http.ResponseWriter, code int, msg string)
+// respondWithJSON(w http.ResponseWriter, code int, payload interface{})
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
--- main.go
+++ main.go
@@ -3,47 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync/atomic"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	w.WriteHeader(http.StatusOK)
-
-	w.Write([]byte("OK"))
-}
-
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	hits := cfg.fileserverHits.Load()
-	htmlTempl := `
-		<html>
-			<body>
-				<h1>Welcome, Chirpy Admin</h1>
-				<p>Chirpy has been visited %d times!</p>
-			</body>
-		</html>
-	`
-	html := fmt.Sprintf(htmlTempl, hits)
-	w.Write([]byte(html))
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("Hits counter reset"))
-}
 
 func main() {
 	cfg := &apiConfig{}
@@ -55,6 +15,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthCheckHandler)
 	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
 	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validationHandler)
 
 	server := &http.Server{
 		Handler: mux,
