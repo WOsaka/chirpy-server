c8e43e4c734c780971d234d72737a3b855b3ac37
WOsaka <wresinski@gmx.net>
2025-06-13 16:23:09 +0200

    create user

--- handler.go
+++ handler.go
@@ -7,10 +7,23 @@ import (
 	"net/http"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/WOsaka/chirpy-server/internal/database"
+	"github.com/google/uuid"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,7 +50,12 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		http.Error(w, "Reset is only allowed in development mode", http.StatusForbidden)
+		return
+	}
 	cfg.fileserverHits.Store(0)
+	cfg.db.DeleteAllUsers(r.Context())
 	w.Write([]byte("Hits counter reset"))
 }
 
@@ -69,6 +87,47 @@ func validationHandler(w http.ResponseWriter, r *http.Request) {
 		CleanedBody: cleaned,
 	}
 	respondWithJSON(w, http.StatusOK, respBody)
+}
+
+func (cfg *apiConfig) userHandler(w http.ResponseWriter, r *http.Request) {
+	var params struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Invalid request body")
+		return
+	}
+
+	if params.Email == "" {
+		respondWithError(w, http.StatusBadRequest, "Email is required")
+		return
+	}
+
+	dbUser, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Failed to create user")
+		return
+	}
+
+	user := User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+
+	if err := respondWithJSON(w, http.StatusCreated, user); err != nil {
+		log.Printf("Error responding with JSON: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) error {
--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/WOsaka/chirpy-server/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	cfg := &apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+
+	cfg := &apiConfig{
+		db: database.New(db),
+		platform: os.Getenv("PLATFORM"),
+	}
+
 	mux := http.NewServeMux()
 	mux.Handle(
 		"/app/",
@@ -16,6 +34,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
 	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 	mux.HandleFunc("POST /api/validate_chirp", validationHandler)
+	mux.HandleFunc("POST /api/users", cfg.userHandler)
 
 	server := &http.Server{
 		Handler: mux,
